171a2e9613dac218cc3064009d599c12caff9031
Phillip Gibson <gibson.phillip.j@gmail.com>
2018-10-30 10:40:26 -0400

    Refactor types

--- types.js
+++ types.js
@@ -1,38 +1,45 @@
 const check = require('./index')
 
+// any non null value
+const Any = i => i != null
+
+// primitives
+const Str = i => Any(i) && (i).constructor === String
+const Num = i => Any(i) && (i).constructor === Number
+const Bool = i => Any(i) && (i).constructor === Boolean
+const Hash = i => Any(i) && (i).constructor === Object
+const Dat = i => Any(i) && (i).constructor === Date
+const Fun = i => Any(i) && (i).constructor === Function
+
+// number types
+const Int = Number.isInteger
+const Float = i => Num(i) && !Number.isInteger(i)
+
+// complex types
+const Arr = type => i => Array.isArray(i) && i.every(type)
+const Obj = o => i => Any(i) && (i).constructor === Object && check(i, o)
+
+// comparative types
+const GT = (n, type) => i => type(i) && i > n
+const GTE = (n, type) => i => type(i) && i >= n
+const LT = (n, type) => i => type(i) && i < n
+const LTE = (n, type) => i => type(i) && i <= n
+const EQ = (n, type) => i => type(i) && i == n
+const EQQ = (n, type) => i => type(i) && i === n
+const NEQ = (n, type) => i => type(i) && i != n
+const NEQQ = (n, type) => i => type(i) && i !== n
+const Or = (...types) => i => types.some(t => t(i))
+const Not = (...types) => i => Any(i) && types.every(t => !t(i))
+const Enum = (...values) => i => values.includes(i)
+
+// optional property
+const Optional = type => i => i == null || type(i)
+
 module.exports = {
-  // literally anything
-  Any: i => i != null,
-
-  // primitives
-  Str: i => i != null && (i).constructor === String,
-  Num: i => i != null && (i).constructor === Number,
-  Bool: i => i != null && (i).constructor === Boolean,
-
-  // number types
-  Int: Number.isInteger,
-  Float: i => i != null && (i).constructor === Number && !Number.isInteger(i),
-
-  // complex types
-  Arr: type => i => Array.isArray(i) && i.every(type),
-  Obj: o => i => i != null && (i).constructor === Object && check(i, o),
-  Hash: i => i != null && (i).constructor === Object,
-  Dat: i => i != null && (i).constructor === Date,
-  Fun: i => i != null && (i).constructor === Function,
-
-  // comparative types
-  GT: (n, type) => i => type(i) && i > n,
-  GTE: (n, type) => i => type(i) && i >= n,
-  LT: (n, type) => i => type(i) && i < n,
-  LTE: (n, type) => i => type(i) && i <= n,
-  EQ: (n, type) => i => type(i) && i == n,
-  EQQ: (n, type) => i => type(i) && i === n,
-  NEQ: (n, type) => i => type(i) && i != n,
-  NEQQ: (n, type) => i => type(i) && i !== n,
-  Or: (...types) => i => types.some(t => t(i)),
-  Not: (...types) => i => i != null && types.every(t => !t(i)),
-  Enum: (...values) => i => values.includes(i),
-
-  // optional property
-  Optional: type => i => i == null || type(i)
+  Any,
+  Str, Num, Bool, Hash, Dat, Fun,
+  Int, Float,
+  Arr, Obj,
+  GT, GTE, LT, LTE, EQ, EQQ, NEQ, NEQQ, Or, Not, Enum,
+  Optional
 }
